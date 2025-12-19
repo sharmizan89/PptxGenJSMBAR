1feb2bc423e001efe59b45b917c178903be2090d
sharmizan89 <mizan.sunlay@gmail.com>
2025-12-19 16:01:44 +0800

    Add files via upload

---
+++ dist/cust-xml-slide-layout-rels.d.ts
@@ -0,0 +1,4 @@
+export declare const CUSTOM_SLIDE_LAYOUT_RELS: {
+    id: number;
+    relsXml: string;
+}[];
---
+++ dist/cust-xml-slide-layouts.d.ts
@@ -0,0 +1,5 @@
+export declare const CUSTOM_SLIDE_LAYOUT_DEFS: {
+    id: number;
+    name: string;
+    xml: string;
+}[];
